c0e784c37ea5a10aa66ce26b5a7b7252b6d34a97
Jesse Cheng <811337@gmail.com>
2019-02-02 14:27:22 -0600

    initial save

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+function letter() {
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    /*
+    based on source from:
+    https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    */
+    for (var i = 0; i < 1; i++)
+      text = text + possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+}
+
+console.log(letter());
+
+var wins=42;
+
+var losses=10;
+
+if (x=random) {
+    var left=left-1;
+}
+
+var guesses=60;
